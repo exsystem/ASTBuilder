4e9bf32bf0e89e3e59dd5b7774ef7d2729b104d9
許子健 <exsystemchina@gmail.com>
2023-11-15 20:18:57 +0800

    測試用例細化
    * 測試用例細化。

--- Test/TestCase/test.pp
+++ Test/TestCase/test.pp
@@ -7,6 +7,18 @@ Begin
   {hello}
   If (mFoo > 12.345) Or (mBar = 'test') Then
   Begin
+    mBar := <<<barbar
+function foo(x: Integer): String;
+begin
+  if (mFoo > 345.12) or (mBar = 'haha') then 
+  begin
+    result := 'whatever'; 
+  end;
+  result := <<<foo
+.... :-) ....
+foo;
+end;
+barbar;
     For I := 100 Downto -100 Do
     Begin
       P := Func1(I);
