5218c3e3f230790f26e247da5dff31af2eb2e5b2
許子健 <exsystemchina@gmail.com>
2024-01-20 20:42:44 +0800

    添加主程序，支持調用動態庫作為詞法解析外掛。
    + 添加主程序，支持調用動態庫作為詞法解析外掛。

--- MyFormatter.lpr
+++ MyFormatter.lpr
@@ -5,10 +5,11 @@ Program MyFormatter;
 Uses
   Test1 In 'Test/Test1.pas',
   Test2 In 'Test/Test2.pas',
-  Test3 In 'Test/Test3.pas';
+  Test3 In 'Test/Test3.pas',
+  MAIN In 'MAIN.PAS';
 
 // {$R *.res}
 
 Begin
-  Test3.Test();
+  MAIN.Run;
 End.
--- Test/Test3.pas
+++ Test/Test3.pas
@@ -21,7 +21,54 @@ Uses {$IFNDEF FPC}
   Lexer,
   CPARSER,
   Parser, GrmRUnit, ASTNode, ParseTr,
- {$IFDEF USE_STRINGS}strings,{$ENDIF} STREAM, GLEXER, GPARSER, CLEXER;
+ {$IFDEF USE_STRINGS}strings,{$ENDIF} STREAM, GLEXER, GPARSER, CLEXER, GRMRNODE, TYPEDEF, StrUtil;
+
+Var
+  mHereDocId: PChar;
+
+Procedure TCodeLexer_Init(Var Self: PCodeLexer);
+{$IFDEF VINTAGE} Far; {$ENDIF}
+Begin
+  TGrammarNode_RegisterTermRule(Self^.GrammarNode, '@HEREDOC_END');
+End;
+
+Procedure TCodeLexer_ProcessToken(Var Self: PCodeLexer);
+{$IFDEF VINTAGE} Far; {$ENDIF}
+Var
+  mMode: PChar;
+  mTokenValue: PChar;
+  mKind: PChar;
+Begin
+  mMode := PPChar(TStack_Top(Self^.Mode))^;
+  mTokenValue := Self^.Parent.CurrentToken.Value;
+  mKind := TGrammarNode_GetTermRuleName(Self^.GrammarNode,
+    PTermRule(Self^.Parent.CurrentToken.Kind)^);
+
+  If StrComp(mMode, 'hereDoc') <> 0 Then
+  Begin
+    Exit;
+  End;
+  If StrComp(mKind, 'START_HEREDOC') = 0 Then
+  Begin
+    mHereDocId := SubStr(mTokenValue, 3, StrLen(mTokenValue) - 3);
+    Exit;
+  End;
+  If StrComp(mKind, 'HEREDOC_TEXT') = 0 Then
+  Begin
+    If StrPos(mTokenValue, mHereDocId) = mTokenValue Then
+    Begin
+      PTermRule(Self^.Parent.CurrentToken.Kind)^ :=
+        TGrammarNode_GetTermRuleId(Self^.GrammarNode, '@HEREDOC_END');
+      TLexer_Retract(PLexer(Self), StrLen(Self^.Parent.CurrentToken.Value) -
+        StrLen(mHereDocId));
+      FreeStr(Self^.Parent.CurrentToken.Value);
+      Self^.Parent.CurrentToken.Value := StrNew(mHereDocId);
+      TCodeLexer_PopMode(PLexer(Self));
+      FreeStr(mHereDocId);
+    End;
+    Exit;
+  End;
+End;
 
 Procedure Test;
 Var
@@ -52,6 +99,8 @@ Begin
   End;
 
   TCodeLexer_Create(mLexer, mCode);
+  mLexer^.OnInit := TCodeLexer_Init;
+  mLexer^.OnProcessToken := TCodeLexer_ProcessToken;
   TAstViewer_Create(mViewer, PLexer(mLexer));
   mParser^.Ast^.VMT^.Accept(mParser^.Ast, mViewer^.As_IAstVisitor);
   mViewer^.Level := 0;
